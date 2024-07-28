3997dff98726f18b8c15cadc449d42ef146a4849
HmingWang <whaim1986@gmail.com>
2024-07-28 18:08:20 +0800

    up

--- src/main/java/com/whaim/j2eetest/HelloServlet.java
+++ src/main/java/com/whaim/j2eetest/HelloServlet.java
@@ -1,10 +1,19 @@
 package com.whaim.j2eetest;
 
 import java.io.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+import javax.naming.NamingException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.naming.InitialContext;
+
 public class HelloServlet extends HttpServlet {
     private String message;
 
@@ -15,11 +24,34 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        // Hello
+        try {
+            InitialContext icx=new InitialContext();
+
+//            String jndi_common = String.format("ejblocal:%sCommonService#cn.cncc.cbsp.bp.common.service.CommonService",
+//                    SysConfig.getProperty("cbsp.bp.ejblocal.path", "cbspbp/cbspbp.war/"));
+
+
+            JndiDemo obj=(JndiDemo) icx.lookup("java:global/j2eetest-1.0/JndiDemo!com.whaim.j2eetest.JndiDemo");
+            if(obj==null){
+                out.println("not found object : jndidemo");
+                return ;
+            }
+//            Method method = obj.getClass().getMethod("sayHello");
+
+            out.println("<h1>we success find the object</h1>");
+
+//            out.println("<h1>" + method.invoke(obj) + "</h1>");
+            out.println("<h1>" + obj.sayHello() + "</h1>");
+            out.println("</body></html>");
+
+        } catch (NamingException  e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
     public void destroy() {
--- src/main/java/com/whaim/j2eetest/JndiDemo.java
+++ src/main/java/com/whaim/j2eetest/JndiDemo.java
@@ -1,2 +1,12 @@
-package com.whaim.j2eetest;public class JndiDemo {
+package com.whaim.j2eetest;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class JndiDemo {
+
+    public String sayHello(){
+        return "Hello jndi World!";
+    }
+
 }
